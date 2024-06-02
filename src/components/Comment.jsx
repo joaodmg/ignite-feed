1f5faf8df25871bc0b48de1b3104f6289a9acdc3
João Vitor Godinho <joaovitordmg@gmail.com>
2024-06-02 08:41:51 -0300

    Adicionando funcionalidade de remover comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import styles from "./Comment.module.css";
 
 import { Avatar } from "./Avatar";
 
-export function Comment({ content }){
+export function Comment({ content, onDeleteComment }){
+  
+  function handleDeleteComment () {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/joaodmg.png" />
@@ -20,7 +25,7 @@ export function Comment({ content }){
                 Cerca de 1h atrás
               </time>
             </div>
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,91 +1,109 @@
-import { format, formatDistanceToNow  } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR' 
+import { format, formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
 
-import { Comment } from './Comment'
-import { Avatar } from './Avatar'
+import { Comment } from "./Comment"
+import { Avatar } from "./Avatar"
 
-import styles from './Post.module.css'
-import { useState } from 'react'
-
+import styles from "./Post.module.css"
+import { useState } from "react"
 
 export function Post({ author, publishedAt, content }) {
-    const [comments, setComments] = useState([
-        "Post muito bacana hein!"
-    ])
+  const [comments, setComments] = useState(["Post muito bacana hein!"])
 
-    const [newCommentText, setNewCommentText] = useState('')
+  const [newCommentText, setNewCommentText] = useState("")
 
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-        locale: ptBR,
-    })
-
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale: ptBR,
-        addSuffix: true,
-    })
-
-    function handleCreateNewComment() {
-        event.preventDefault()
-
-
-        setComments([...comments, newCommentText])
-        setNewCommentText('')
-
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
     }
+  )
 
-    function handleNewCommentChange() {
-        setNewCommentText(event.target.value)
-    }
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
 
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                <Avatar src={author.avatarUrl}/>
+  function handleCreateNewComment() {
+    event.preventDefault()
 
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
+    setComments([...comments, newCommentText])
+    setNewCommentText("")
+  }
 
-                <time 
-                title={publishedDateFormatted} 
-                dateTime={publishedAt.toISOString()}
-                >
-                    {publishedDateRelativeToNow}
-                </time>
-            </header>
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
 
-            <div className={styles.content}>
-                {content.map(line => {
-                    if (line.type == 'paragraph') {
-                        return <p>{line.content}</p>
-                    } else if (line.type == 'link'){
-                        return <p><a href='#'>{line.content}</a></p>
-                    }
-                })}
-            </div>
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+        return comment !== commentToDelete
+        /* Com o filter ele ira retornar apenas os comentários que 
+         forem diferentes do comentário que estamos recebendo como parametro */
+    })
+    setComments(commentsWithoutDeletedOne)
+  }
 
-            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
 
-                <textarea
-                    name='comment' 
-                    placeholder='Deixe um comentário'
-                    value={newCommentText}
-                    onChange={handleNewCommentChange}
-                />
-                <footer>
-                    <button type='submit'>Publicar</button>
-                </footer>
-            </form>
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-            <div className={styles.commentList}>
-                {comments.map(coment => {
-                    return <Comment content={coment}/>
-                })}
-            </div>
-        </article>
-    )
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type == "paragraph") {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type == "link") {
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            )
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map((coment) => {
+          return (
+            <Comment
+              content={coment}
+              key={coment}
+              onDeleteComment={deleteComment}
+            />
+          )
+        })}
+      </div>
+    </article>
+  )
 }
